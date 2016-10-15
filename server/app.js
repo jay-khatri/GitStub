6d509fc1166380fda894904580d5e8231ff10555
Jay Khatri <khatri.florida@gmail.com>
2016-10-15 02:29:48 -0500

    updated server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 express = require('express');
 var app = express();
+var request = require('request');
 
 app.get('/recommend', function (req, res) {
-	console.log(req.query);
+	console.log(req.query.repo);
+	if(req.query.repo === undefined){
+			console.log("undefined issue");
+	}else{
+			request.get("https://api.github.com/repos/" + req.query.repo + "/contributors", function(error,response,body){
+			console.log(response.statusCode);
+				if (!error && response.statusCode == 200) {
+			        console.log(body); // Show the HTML for the Modulus homepage.
+					console.log(response);
+			    }});
+	}
 });
 
 app.listen(3000, function () {
